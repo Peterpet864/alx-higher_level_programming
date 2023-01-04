852b907a164ce29bf3ebef5765ec3991112e00c8
Peterpet864 <Peterokon86@yahoo.com>
2023-01-04 23:18:11 +0100

    python

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-# 10-add.py
-# Amos Mwongela Gabriel <amosmwongelah@gmail.com>
-
-
 def add(a, b):
-    """Return the addition of a and b."""
-    return (a + b)  
+    return a + b
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
-for x in range(ord("a"), ord("z") + 1):
-    if chr(x) != 'q' and chr(x) != 'e':
-        print("{}".format(chr(x)), end="")
-
+for i in range(0, 26):
+    if i == 4 or i == 16:
+        continue
+    else:
+        print("{:s}".format(chr(ord('a') + i),), end="")
